1a13783038a6d0b6a85af022ee6973ee2937e8e1
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2020-06-02 13:34:25 +0300

    Add files via upload

---
+++ CS/DXGridGetStarted/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpf.Core;
+
+namespace DXGridGetStarted {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS/DXGridGetStarted/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+
+namespace DXGridGetStarted {
+    public class ViewModel : ViewModelBase {
+
+        NorthwindEntities northwindDBContext;
+        public ViewModel() {
+            if (IsInDesignMode) {
+                Orders = new ObservableCollection<Order>();
+                Shippers = new ObservableCollection<Shipper>();
+            }
+            else {
+                northwindDBContext = new NorthwindEntities();
+
+                northwindDBContext.Orders.Load();
+                Orders = northwindDBContext.Orders.Local;
+
+                northwindDBContext.Shippers.Load();
+                Shippers = northwindDBContext.Shippers.Local;
+            }
+            ValidateAndSaveCommand = new DelegateCommand(ValidateAndSave);
+        }
+        public ObservableCollection<Order> Orders {
+            get => GetValue<ObservableCollection<Order>>();
+            private set => SetValue(value);
+        }
+        public ObservableCollection<Shipper> Shippers {
+            get => GetValue<ObservableCollection<Shipper>>();
+            private set => SetValue(value);
+        }
+        public DelegateCommand ValidateAndSaveCommand { get; private set; }
+        void ValidateAndSave() {
+            northwindDBContext.SaveChanges();
+        }
+    }
+}
