09a587190309d9f4b0639c2d5e4db30b5d36814e
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2020-07-03 10:40:05 +0300

    Update ViewModel.cs

--- CS/WPFDataGridGettingStartedNETFramework/ViewModel.cs
+++ CS/WPFDataGridGettingStartedNETFramework/ViewModel.cs
@@ -22,8 +22,14 @@ namespace WPFDataGridGettingStartedNETFramework {
             }
             ValidateAndSaveCommand = new DelegateCommand(ValidateAndSave);
         }
-        public ObservableCollection<Order> Orders { get; }
-        public ObservableCollection<Shipper> Shippers { get; }
+        public ObservableCollection<Order> Orders {
+            get => GetValue<ObservableCollection<Order>>();
+            private set => SetValue(value);
+        }
+        public ObservableCollection<Shipper> Shippers {
+            get => GetValue<ObservableCollection<Shipper>>();
+            private set => SetValue(value);
+        }
         public DelegateCommand ValidateAndSaveCommand { get; }
         void ValidateAndSave() {
             northwindDBContext.SaveChanges();
