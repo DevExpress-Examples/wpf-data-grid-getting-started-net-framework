4743dab0c3f4c44887f6a8a58bc37c463b15480d
DevExpressExampleBot <fakeEmail>
2021-12-15 14:35:55 +0300

    Source auto update [skip ci]

--- VB/WPFDataGridGettingStartedNETFramework/MainWindow.xaml.vb
+++ VB/WPFDataGridGettingStartedNETFramework/MainWindow.xaml.vb
@@ -6,7 +6,7 @@ Namespace WPFDataGridGettingStartedNETFramework
         Inherits ThemedWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
